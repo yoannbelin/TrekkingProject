3d5f772029c989f97924cdd6c5f7f1482e0248da
yoannbelin <yoann.belin@icloud.com>
2018-01-03 18:23:05 +0100

    Object LoadTreksManager done (Yoann)

--- 3_Nodejs/2_Server/ArchiNodeJS_v1.7.1/src/loadTreks.js
+++ 3_Nodejs/2_Server/ArchiNodeJS_v1.7.1/src/loadTreks.js
@@ -1,133 +1,134 @@
-var LoadTreksManager = new Object();
+var LoadTreksManager = {
 
-LoadTreksManager._treks = [];
+    _treks: [],
 
-//
-// Build combobox
-//
-LoadTreksManager.buildCombo = function() {
-    var html = "";
-    html += "<option disabled selected>" + "Selectionner votre trek" + "</option>"
-    _treks.forEach((option) => {
-        html += "<option data-id= " + option.id + ">" + option.label + "</option>";
-    })
-    document.getElementById("idCombo").innerHTML = html;
-}
+    //
+    // Build combobox
+    //
+    buildCombo: function () {
+        var html = "";
+        html += "<option disabled selected>" + "Selectionner votre trek" + "</option>"
+        this._treks.forEach((option) => {
+            html += "<option data-id= " + option.id + ">" + option.label + "</option>";
+        })
+        document.getElementById("idCombo").innerHTML = html;
+    },
 
-//
-//Show trek's characteritics
-//
-LoadTreksManager.infoTrek = function(i) {
-    LoadTreksManager.show_btn('.raz');
-    LoadTreksManager.show_btn('.detail');
-    var html = "";
-    html += "<tr>";
-    html += "<td> Nom du trek </td>";
-    html += "<td> Distance du trek </td>";
-    html += "<td> Temps estimés </td>";
-    html += "<td> Niveau de difficulté </td>";
-    html += "</tr>";
-    html += "<tr>";
-    html += "<td>" + _treks[i].label + "</td>";
-    html += "<td>" + _treks[i].length + "</td>";
-    html += "<td>" + _treks[i].time + "</td>";
-    html += "<td>" + _treks[i].level + "</td>";
-    html += "</tr>";
-    document.getElementById('idTableau').innerHTML = html;
+    //
+    //Show trek's characteritics
+    //
+    infoTrek: function (i) {
+        this.show_btn('.raz');
+        this.show_btn('.detail');
+        var html = "";
+        html += "<tr>";
+        html += "<td> Nom du trek </td>";
+        html += "<td> Distance du trek </td>";
+        html += "<td> Temps estimés </td>";
+        html += "<td> Niveau de difficulté </td>";
+        html += "</tr>";
+        html += "<tr>";
+        html += "<td>" + this._treks[i].label + "</td>";
+        html += "<td>" + this._treks[i].length + "</td>";
+        html += "<td>" + this._treks[i].time + "</td>";
+        html += "<td>" + this._treks[i].level + "</td>";
+        html += "</tr>";
+        document.getElementById('idTableau').innerHTML = html;
 
-    var datas = JSON.parse(_treks[i].pathway)
-    drawTrek(datas);
-}
+        var datas = JSON.parse(this._treks[i].pathway)
+        drawTrek(datas);
+    },
 
-//
-// Add trek by drawing with Leaflet and input html
-//
-LoadTreksManager.createNewTrek = function() {
+    //
+    // Add trek by drawing with Leaflet and input html
+    //
+    createNewTrek: function () {
 
-    addDrawTools();
+        addDrawTools();
 
-    var html = "";
-    html += "<tr>";
-    html += "<td> Nom du trek </td>";
-    html += "<td> Distance du trek </td>";
-    html += "<td> Temps estimés </td>";
-    html += "<td> Niveau de difficulté </td>";
-    html += "</tr>";
+        var html = "";
+        html += "<tr>";
+        html += "<td> Nom du trek </td>";
+        html += "<td> Distance du trek </td>";
+        html += "<td> Temps estimés </td>";
+        html += "<td> Niveau de difficulté </td>";
+        html += "</tr>";
 
-    html += "<tr>";
-    html += "<td>" + '<textarea name="" id="nom"></textarea>' + "</td>";
-    html += "<td>" + '<textarea name="" id="distance", readonly></textarea>' + "</td>";
-    html += "<td>" + '<textarea  name="" id="temps"></textarea>' + "</td>";
-    html += "<td>" + '<textarea  name="" id="difficulte"></textarea>' + "</td>";
-    html += "</tr>";
+        html += "<tr>";
+        html += "<td>" + '<textarea name="" id="nom"></textarea>' + "</td>";
+        html += "<td>" + '<textarea name="" id="distance", readonly></textarea>' + "</td>";
+        html += "<td>" + '<textarea  name="" id="temps"></textarea>' + "</td>";
+        html += "<td>" + '<textarea  name="" id="difficulte"></textarea>' + "</td>";
+        html += "</tr>";
 
-    document.getElementById('idTableau').innerHTML = html;
+        document.getElementById('idTableau').innerHTML = html;
 
-    LoadTreksManager.show_btn('.save');
-    LoadTreksManager.show_btn('.raz');
-    LoadTreksManager.hide_btn('.detail');
-    LoadTreksManager.hide_btn('.newtrek');
-}
+        this.show_btn('.save');
+        this.show_btn('.raz');
+        this.hide_btn('.detail');
+        this.hide_btn('.newtrek');
+    },
 
+    //
+    // Load trek list in combobox
+    //
+    loadTreks: function () {
+        $http.get('/api-rest/treks', function (res) {
+            LoadTreksManager._treks = res;
+            LoadTreksManager.buildCombo();
+        }, 'json');
+        this.show_btn('.newtrek');
+    },
 
-//
-// Load trek list in combobox
-//
-LoadTreksManager.loadTreks = function() {
-    $http.get('/api-rest/treks', function(res) {
-        _treks = res;
-        LoadTreksManager.buildCombo();
-    }, 'json');
-    LoadTreksManager.show_btn('.newtrek');
-}
+    //
+    // Add trek
+    //
+    addTrek: function () {
+        this.hide_btn('.save');
+        this.show_btn('.newtrek');
 
-//
-// Add trek
-//
-LoadTreksManager.addTrek = function() {
-    LoadTreksManager.hide_btn('.save');
-    LoadTreksManager.show_btn('.newtrek');
+        let trek_path = document.chemin_trek;
 
-    let trek_path = document.chemin_trek;
+        var input = {
+            'label': document.getElementById('nom').value,
+            'length': document.getElementById('distance').value,
+            'time': document.getElementById('temps').value,
+            'level': document.getElementById('difficulte').value,
+            'pathway': { 'chemin': trek_path }
+        };
 
-    var input = {
-        'label': document.getElementById('nom').value,
-        'length': document.getElementById('distance').value,
-        'time': document.getElementById('temps').value,
-        'level': document.getElementById('difficulte').value,
-        'pathway': { 'chemin': trek_path }
-    };
+        $http.post('/api-rest/treks', input, function (res) {
+            if (res.trek.id !== 0) {
+                this._treks.push(res.trek);
+            }
+            this.infoTrek(this._treks.length - 1);
+        }, 'json');
+        document.getElementById('messageToUser').innerHTML = 'success!';
+        this.loadTreks();
+    },
 
-    $http.post('/api-rest/treks', input, function(res) {
-        if (res.trek.id !== 0) {
-            _treks.push(res.trek);
+    //
+    // Hide / Show drawing buttons
+    //
+    hide_btn: function (id) {
+        if (document.querySelector(id).style.visibility === "visible") {
+            document.querySelector(id).style.visibility = "";
         }
-        LoadTreksManager.infoTrek(_treks.length - 1);
-    }, 'json');
-    document.getElementById('messageToUser').innerHTML = 'success!';
-    LoadTreksManager.loadTreks();
-}
+    },
 
-//
-// Hide / Show drawing buttons
-//
-LoadTreksManager.hide_btn = function(id) {
-    if (document.querySelector(id).style.visibility === "visible") {
-        document.querySelector(id).style.visibility = "";
+    show_btn: function (id) {
+        if (document.querySelector(id).style.visibility === "") {
+            document.querySelector(id).style.visibility = "visible";
+        }
+    },
+
+    //
+    // Reset Page
+    //
+    resetPage: function () {
+        window.location.reload();
     }
 };
-
-LoadTreksManager.show_btn = function(id) {
-    if (document.querySelector(id).style.visibility === "") {
-        document.querySelector(id).style.visibility = "visible";
-    }
-};
-
-//
-// Reset Page
-//
-LoadTreksManager.resetPage = function() {
-    window.location.reload();
-}
 
 window.onload = LoadTreksManager.loadTreks();
+
